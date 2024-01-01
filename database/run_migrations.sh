318c9f42116608dc7ef53d85d9d8fc24cd4e6598
hekolcu <hekolcu@gmail.com>
2024-01-01 17:19:08 +0300

    changed the migrations script to always use vitamis db

--- database/run_migrations.sh
+++ database/run_migrations.sh
@@ -5,7 +5,6 @@ echo "Please enter the database credentials:"
 read -p "Database Username: " DB_USER
 read -s -p "Database Password: " DB_PASS
 echo
-read -p "Database Name: " DB_NAME
 
 # Define the directory containing migration files
 MIGRATION_DIR="$(dirname "$0")/Migrations"
@@ -14,7 +13,7 @@ MIGRATION_DIR="$(dirname "$0")/Migrations"
 echo "Running migration files..."
 for sql_file in `ls $MIGRATION_DIR/*.sql | sort -V`; do
     echo "Processing $sql_file..."
-    mysql -u "$DB_USER" -p"$DB_PASS" "$DB_NAME" < "$sql_file"
+    mysql -u "$DB_USER" -p"$DB_PASS" "vitamis" < "$sql_file"
     if [ $? -ne 0 ]; then
         echo "An error occurred with $sql_file - stopping migration."
         exit 1
