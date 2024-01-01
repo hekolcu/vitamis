deab29284f045bb8e6a4426caecaaf4f7c620bc4
hekolcu <hekolcu@gmail.com>
2024-01-01 17:03:16 +0300

    initial user table

---
+++ database/Migrations/20240101_users_table_intial_scheme.sql
@@ -0,0 +1,19 @@
+-- initial_scheme.sql
+
+-- Use the appropriate database
+USE vitamis_db;
+
+-- Create Users table
+CREATE TABLE Users (
+    UserID INT AUTO_INCREMENT PRIMARY KEY,
+    Username VARCHAR(50) NOT NULL,
+    Password VARCHAR(255) NOT NULL,
+    UserType ENUM('Advisee', 'Dietitian', 'Academician Dietitian', 'Admin') NOT NULL,
+    Email VARCHAR(100) UNIQUE NOT NULL,
+    Gender ENUM('Male', 'Female', 'Other') NOT NULL,
+    DateOfBirth DATE NOT NULL,
+    HealthConditions TEXT,
+    CHECK (DateOfBirth >= '1900-01-01' AND DateOfBirth <= CURDATE())
+);
+
+COMMIT;
