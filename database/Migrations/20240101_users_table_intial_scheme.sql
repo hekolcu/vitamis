ed21daf6733f2a600d6bfb2573cde56ce44350ad
hekolcu <hekolcu@gmail.com>
2024-01-01 17:12:20 +0300

    updated working users table creation

--- database/Migrations/20240101_users_table_intial_scheme.sql
+++ database/Migrations/20240101_users_table_intial_scheme.sql
@@ -1,19 +1,19 @@
--- initial_scheme.sql
+-- users_table_initial_scheme.sql
 
 -- Use the appropriate database
-USE vitamis_db;
+USE vitamis;
 
 -- Create Users table
 CREATE TABLE Users (
-    UserID INT AUTO_INCREMENT PRIMARY KEY,
-    Username VARCHAR(50) NOT NULL,
-    Password VARCHAR(255) NOT NULL,
-    UserType ENUM('Advisee', 'Dietitian', 'Academician Dietitian', 'Admin') NOT NULL,
-    Email VARCHAR(100) UNIQUE NOT NULL,
-    Gender ENUM('Male', 'Female', 'Other') NOT NULL,
-    DateOfBirth DATE NOT NULL,
-    HealthConditions TEXT,
-    CHECK (DateOfBirth >= '1900-01-01' AND DateOfBirth <= CURDATE())
+   UserID INT AUTO_INCREMENT PRIMARY KEY,
+   Username VARCHAR(50) NOT NULL,
+   Password VARCHAR(255) NOT NULL,
+   UserType ENUM('Advisee', 'Dietitian', 'Academician Dietitian', 'Admin') NOT NULL,
+   Email VARCHAR(100) UNIQUE NOT NULL,
+   Gender ENUM('Male', 'Female', 'Other') NOT NULL,
+   DateOfBirth DATE NOT NULL,
+   HealthConditions TEXT,
+   CHECK (DateOfBirth >= '1900-01-01')
 );
 
 COMMIT;
