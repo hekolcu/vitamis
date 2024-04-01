ac5c3fbf6c2127cf23ffc3030d0bbcedfe19f6a3
Alper Avcı <adnanalperavci@gmail.com>
2024-04-01 16:03:25 +0300

    data collection for every category

--- dataCollection/food_nutritional_values.py
+++ dataCollection/food_nutritional_values.py
@@ -8,55 +8,54 @@ import time
 # Data taken from Türkomp at 21.01.2024
 ###################################
 driver = webdriver.Chrome()
-url_meyve = 'https://turkomp.tarimorman.gov.tr/database?type=foods&group=9type=foods&group=9'
-url_sebze = 'https://turkomp.tarimorman.gov.tr/database?type=foods&group=8'
-# Open the website
-driver.get(url_meyve)
+urls = [
+    'https://turkomp.tarimorman.gov.tr/database?type=foods&group=1',
+    'https://turkomp.tarimorman.gov.tr/database?type=foods&group=2',
+    'https://turkomp.tarimorman.gov.tr/database?type=foods&group=3',
+    'https://turkomp.tarimorman.gov.tr/database?type=foods&group=4',
+    'https://turkomp.tarimorman.gov.tr/database?type=foods&group=5',
+    'https://turkomp.tarimorman.gov.tr/database?type=foods&group=6',
+    'https://turkomp.tarimorman.gov.tr/database?type=foods&group=7',
+    'https://turkomp.tarimorman.gov.tr/database?type=foods&group=8',
+    'https://turkomp.tarimorman.gov.tr/database?type=foods&group=9',
+    'https://turkomp.tarimorman.gov.tr/database?type=foods&group=10',
+    'https://turkomp.tarimorman.gov.tr/database?type=foods&group=11',
+    'https://turkomp.tarimorman.gov.tr/database?type=foods&group=12',
+    'https://turkomp.tarimorman.gov.tr/database?type=foods&group=13',
+    'https://turkomp.tarimorman.gov.tr/database?type=foods&group=14'
+]
 wait = WebDriverWait(driver, 10)
-
 table_data = []
-#All foods table
-allTable = driver.find_element(By.ID, 'mydatalist')
-allRows = allTable.find_elements(By.TAG_NAME,'tr')
-rowIndex=1
-while(rowIndex<len(allRows)):
-    time.sleep(1)
-    link=driver.find_element(By.XPATH,f"/html[1]/body[1]/div[2]/div[3]/div[1]/div[2]/div[2]/div[2]/div[2]/table[1]/tbody[1]/tr[{rowIndex}]/td[1]/a[1]")
-    gida=link.text
-    grup = driver.find_element(By.XPATH, f"/html[1]/body[1]/div[2]/div[3]/div[1]/div[2]/div[2]/div[2]/div[2]/table[1]/tbody[1]/tr[{rowIndex}]/td[3]/a[1]").text
-    grup = grup.replace("» ", "")
-    link.click()
-    table = wait.until(EC.presence_of_element_located((By.ID, 'foodResultlist')))
-    rows = table.find_elements(By.TAG_NAME, 'tr')
-    for row in rows:
-        cells = row.find_elements(By.TAG_NAME, 'td')
-        row_data = [gida, grup]+[cell.text for cell in cells]
-        table_data.append(row_data)
-    driver.back()
-    rowIndex+=1
 
-driver.get(url_sebze)
-allTable = driver.find_element(By.ID, 'mydatalist')
-allRows = allTable.find_elements(By.TAG_NAME,'tr')
-rowIndex=1
-while(rowIndex<len(allRows)):
-    time.sleep(1)
-    link=driver.find_element(By.XPATH,f"/html[1]/body[1]/div[2]/div[3]/div[1]/div[2]/div[2]/div[2]/div[2]/table[1]/tbody[1]/tr[{rowIndex}]/td[1]/a[1]")
-    gida=link.text
-    grup = driver.find_element(By.XPATH, f"/html[1]/body[1]/div[2]/div[3]/div[1]/div[2]/div[2]/div[2]/div[2]/table[1]/tbody[1]/tr[{rowIndex}]/td[3]/a[1]").text
-    grup = grup.replace("» ", "")
-    link.click()
-    table = wait.until(EC.presence_of_element_located((By.ID, 'foodResultlist')))
-    rows = table.find_elements(By.TAG_NAME, 'tr')
-    for row in rows:
-        cells = row.find_elements(By.TAG_NAME, 'td')
-        row_data = [gida, grup]+[cell.text for cell in cells]
-        table_data.append(row_data)
-    driver.back()
-    rowIndex+=1
+def get_data(url):
+    driver.get(url)
+    #All foods table
+    allTable = driver.find_element(By.ID, 'mydatalist')
+    allRows = allTable.find_elements(By.TAG_NAME,'tr')
+    rowIndex=1
+    while(rowIndex<len(allRows)):
+        time.sleep(1)
+        link=driver.find_element(By.XPATH,f"/html[1]/body[1]/div[2]/div[3]/div[1]/div[2]/div[2]/div[2]/div[2]/table[1]/tbody[1]/tr[{rowIndex}]/td[1]/a[1]")
+        gida=link.text
+        id = driver.find_element(By.XPATH, f"/html[1]/body[1]/div[2]/div[3]/div[1]/div[2]/div[2]/div[2]/div[2]/table[1]/tbody[1]/tr[{rowIndex}]/td[2]").text
+        grup = driver.find_element(By.XPATH, f"/html[1]/body[1]/div[2]/div[3]/div[1]/div[2]/div[2]/div[2]/div[2]/table[1]/tbody[1]/tr[{rowIndex}]/td[3]/a[1]").text
+        grup = grup.replace("» ", "")
+        link.click()
+        table = wait.until(EC.presence_of_element_located((By.ID, 'foodResultlist')))
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        for row in rows:
+            cells = row.find_elements(By.TAG_NAME, 'td')
+            row_data = [gida, id, grup]+[cell.text for cell in cells]
+            table_data.append(row_data)
+        driver.back()
+        rowIndex+=1
+
+# Call function
+for url in urls:
+    get_data(url)
 
 driver.quit()
-columns = ["gida","grup","bilesen","birim","ortalama","minimum","maksimin"]
+columns = ["gida","id","grup","bilesen","birim","ortalama","minimum","maksimin"]
 data = table_data[1:]
 df = pd.DataFrame(data, columns=columns)
 df.to_json("food_nutritional_values.json", orient="records", indent=2)
