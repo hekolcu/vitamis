423df8cbd3dce2acf842686e9df71bb4d4e00b1d
Alper Avcı <adnanalperavci@gmail.com>
2024-05-03 14:48:55 +0300

    Revert "changed test language to python"
    This reverts commit 9f51a9ab35274d16ba97b59204918917edfbb960.

--- frontendTests/constants/sign_in_page_constants.py
+++
@@ -1,7 +0,0 @@
-from selenium.webdriver.common.by import By
-class Constants:
-    url = "https://www.vitamis.hekolcu.com/auth/sign-in"
-    email_textbox = ((By.XPATH, "(//input[@name='email'])[1]"), "Email Textbox")
-    password_textbox = ((By.XPATH, "(//input[@name='password'])[1]"), "Password Textbox")
-    sign_in_btn = ((By.XPATH, "//button[@type='submit']"), "Sign In Button")
-    expected_url = "https://www.vitamis.hekolcu.com/dashboard"
