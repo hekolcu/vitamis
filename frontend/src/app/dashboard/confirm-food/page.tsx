f121be68da0595e120924a32ae1fcc291c2f6007
Aybuke Yaren <85608966+AybYrn@users.noreply.github.com>
2024-05-03 19:40:32 +0000

    refactor: Update search food/Confirm food page UI

--- frontend/src/app/dashboard/confirm-food/page.tsx
+++ frontend/src/app/dashboard/confirm-food/page.tsx
@@ -4,22 +4,18 @@ import Stack from '@mui/material/Stack';
 import Typography from '@mui/material/Typography';
 import Grid from '@mui/material/Unstable_Grid2';
 import { config } from '@/config';
-import {ConfirmFoodForm} from '@/components/dashboard/confirm-food/confirm-food-form';
+import { ConfirmFoodForm } from '@/components/dashboard/confirm-food/confirm-food-form';
 
 export const metadata = { title: `Confirm Food | Dashboard | ${config.site.name}` } satisfies Metadata;
 
 export default function Page(): React.JSX.Element {
-    
-    return (
-      <Stack spacing={3}>
-        <div>
-          <Typography variant="h4" color={'#fa8805'} align='center'>Confirm Food Item</Typography>
-        </div>
-        <Grid container spacing={3} direction="row" justifyContent="center" alignItems="center">
-          <Grid lg={8} md={6} xs={12}>
-            <ConfirmFoodForm/>
-          </Grid>
-        </Grid>
-      </Stack>
-    );
-  }
+
+  return (
+    <Stack width={'100%'} spacing={3}>
+      <div>
+        <Typography variant="h4" color={'#fa8805'} align='center'>Confirm Food</Typography>
+      </div>
+      <ConfirmFoodForm />
+    </Stack>
+  );
+}
