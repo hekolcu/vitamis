2f34d1cfca4e72c444ab1af25d3abb257b9f1bec
Aybuke Yaren <85608966+AybYrn@users.noreply.github.com>
2024-05-04 11:50:24 +0000

    refactor: Update search food page UI and component structure

--- frontend/src/components/dashboard/search-food/search-food-form.tsx
+++ frontend/src/components/dashboard/search-food/search-food-form.tsx
@@ -22,12 +22,14 @@ export function SearchForm() {
   const [query, setQuery] = useState('');
   const [searchResults, setSearchResults] = useState<FoodSearchItem[]>([]);
   const [errorMessage, setErrorMessage] = useState<string | null>(null);
+  const [searchCompleted, setSearchCompleted] = useState(false); // new state
 
   React.useEffect(() => {
     console.log(searchResults);
   }, [searchResults]);
 
   const handleSearch = async () => {
+    
     if (!query.trim()) {
       setErrorMessage('Lütfen gerekli alanları doldurunuz');
       return false;
@@ -41,6 +43,8 @@ export function SearchForm() {
       setErrorMessage(null);
     } catch (error) {
       console.error('Error fetching search results:', error);
+    }finally {
+      setSearchCompleted(true);
     }
   };
 
@@ -71,42 +75,45 @@ export function SearchForm() {
         </Grid>
       </Grid>
       <Grid container spacing={2} direction="row" alignItems="center" justifyContent="center">
-        {searchResults.map((result, index) => (
-          <Grid item xs={12} lg={6} md={6} key={index}>
-            <Card style={{ width: '100%', padding: '20px' }}>
-              <Typography variant="h6" color={'#fa8805'} style={{ textAlign: 'left' }}>{result.category}</Typography>
-              <Typography variant="subtitle1" style={{ textAlign: 'left', marginBottom: '5px' }}>{result.name}</Typography>
-              {result.vitamins.length > 0 ? (
-                <TableContainer>
-                  <Table size="small">
-                    <TableHead>
-                      <TableRow>
-                        <TableCell>Vitaminler</TableCell>
-                        <TableCell align="right">Ortalama</TableCell>
-                        <TableCell align="right">Birim</TableCell>
-                      </TableRow>
-                    </TableHead>
-                    <TableBody>
-                      {result.vitamins.map((vitamin, idx) => (
-                        <TableRow key={idx}>
-                          <TableCell component="th" scope="row">
-                            {vitamin.name}
-                          </TableCell>
-                          <TableCell align="right">{vitamin.average}</TableCell>
-                          <TableCell align="right">{vitamin.unit}</TableCell>
+        {searchResults.length > 0 ? (
+          searchResults.map((result, index) => (
+            <Grid item xs={12} lg={6} md={6} key={index}>
+              <Card style={{ width: '100%', padding: '20px' }}>
+                <Typography variant="h6" color={'#fa8805'} style={{ textAlign: 'left' }}>{result.category}</Typography>
+                <Typography variant="subtitle1" style={{ textAlign: 'left', marginBottom: '5px' }}>{result.name}</Typography>
+                {result.vitamins.length > 0 ? (
+                  <TableContainer>
+                    <Table size="small">
+                      <TableHead>
+                        <TableRow>
+                          <TableCell>Vitaminler</TableCell>
+                          <TableCell align="right">Ortalama</TableCell>
+                          <TableCell align="right">Birim</TableCell>
                         </TableRow>
-                      ))}
-                    </TableBody>
-                  </Table>
-                </TableContainer>
-              ) : (
-                <Typography variant="body2" align='center'>Vitamin bilgisi mevcut değil</Typography>
-              )}
-            </Card>
-          </Grid>
-        ))}
+                      </TableHead>
+                      <TableBody>
+                        {result.vitamins.map((vitamin, idx) => (
+                          <TableRow key={idx}>
+                            <TableCell component="th" scope="row">
+                              {vitamin.name}
+                            </TableCell>
+                            <TableCell align="right">{vitamin.average}</TableCell>
+                            <TableCell align="right">{vitamin.unit}</TableCell>
+                          </TableRow>
+                        ))}
+                      </TableBody>
+                    </Table>
+                  </TableContainer>
+                ) : (
+                  <Typography variant="body2" align='center'>Vitamin bilgisi mevcut değil</Typography>
+                )}
+              </Card>
+            </Grid>
+          ))
+        ) : (
+          searchCompleted && <Typography variant="body2" align='center' marginTop={'20px'} color={'red'} fontWeight={'bold'}>Yemek bulunmuyor</Typography>
+        )}
       </Grid>
-
     </Stack>
   );
 }
