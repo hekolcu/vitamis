17cd970cff12b605af3633a4753468afb8d01b92
Aybuke Yaren <85608966+AybYrn@users.noreply.github.com>
2024-05-05 18:20:55 +0000

    Update page title and component labels in My Reports page

--- frontend/src/app/dashboard/my-reports/page.tsx
+++ frontend/src/app/dashboard/my-reports/page.tsx
@@ -6,18 +6,18 @@ import Grid from '@mui/material/Unstable_Grid2';
 import { config } from '@/config';
 import { ViewReport } from '@/components/dashboard/my-reports/my-reports-component';
 
-export const metadata = { title: `My Reports | Dashboard | ${config.site.name}` } satisfies Metadata;
+export const metadata = { title: `Raporlarım | Gösterge Paneli | ${config.site.name}` } satisfies Metadata;
 
 export default function Page(): React.JSX.Element {
     
     return (
       <Stack spacing={3}>
         <div>
-          <Typography variant="h4" color={'#fa8805'} align='center'>My Reports</Typography>
+          <Typography variant="h4" color={'#fa8805'} align='center'>Raporlarım</Typography>
         </div>
         <Grid container spacing={3} direction="row" justifyContent="center" alignItems="center">
-          <Grid lg={8} md={6} xs={12}>
-            <ViewReport></ViewReport>
+          <Grid lg={8} md={8} xs={12}>
+            <ViewReport />
           </Grid>
         </Grid>
       </Stack>
